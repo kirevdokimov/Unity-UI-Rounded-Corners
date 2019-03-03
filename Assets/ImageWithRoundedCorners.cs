b22b79c3c9a43b4be69c73782732f724f5040825
Nobi <nobinator.root@gmail.com>
2019-03-03 19:42:33 +0300

    Script that updates material properties : _Width and _Height and works as simple Image

---
+++ Assets/ImageWithRoundedCorners.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ImageWithRoundedCorners : Image {
+	private static readonly int Width = Shader.PropertyToID("_Width");
+	private static readonly int Height = Shader.PropertyToID("_Height");
+
+	private void OnRenderObject(){
+			this.material.SetFloat(Width,((RectTransform)transform).rect.width);
+			this.material.SetFloat(Height,((RectTransform)transform).rect.height);
+	}
+}
