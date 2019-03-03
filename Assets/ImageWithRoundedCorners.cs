15b239b8dbbfa72ce8f87d8ad601ec1f3bd76f95
Nobi <nobinator.root@gmail.com>
2019-03-03 19:51:25 +0300

    Setter for radius available

--- Assets/ImageWithRoundedCorners.cs
+++ Assets/ImageWithRoundedCorners.cs
@@ -7,6 +7,12 @@ using UnityEngine.UI;
 public class ImageWithRoundedCorners : Image {
 	private static readonly int Width = Shader.PropertyToID("_Width");
 	private static readonly int Height = Shader.PropertyToID("_Height");
+	private static readonly int Radius = Shader.PropertyToID("_Radius");
+
+	// TODO think up a better name
+	public void SetRadius(float radius){
+		this.material.SetFloat(Radius, radius);
+	}
 
 	private void OnRenderObject(){
 			this.material.SetFloat(Width,((RectTransform)transform).rect.width);
