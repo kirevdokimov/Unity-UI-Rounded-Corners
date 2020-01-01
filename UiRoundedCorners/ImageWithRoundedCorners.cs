f18b5576ff289f7c65488e944434f4b769b403d0
Nobi <nobinator.root@gmail.com>
2020-01-01 19:00:11 +0300

    issue#9 Constraints just removed

--- UiRoundedCorners/ImageWithRoundedCorners.cs
+++ UiRoundedCorners/ImageWithRoundedCorners.cs
@@ -19,13 +19,6 @@ public class ImageWithRoundedCorners : MonoBehaviour {
 
 	private void Refresh(){
 		var rect = ((RectTransform) transform).rect;
-		ValidateConstraints(rect);
 		material.SetVector(Props, new Vector4(rect.width, rect.height, radius, 0));
-	}
-
-	private void ValidateConstraints(Rect rect){
-		if (radius > rect.width) radius = rect.width;
-		if (radius > rect.height) radius = rect.height;
-		if (radius < 0) radius = 0;
 	}
 }
