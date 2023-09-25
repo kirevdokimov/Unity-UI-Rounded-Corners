d504912d176066bffc20ba9c8f5bf7e34b55a6f6
雨落随风 <bshsf@qq.com>
2023-09-25 16:44:28 +0800

    call setdirty after create meterial
    fixed a namespace error usage
    fixed code indentation

--- UiRoundedCorners/ImageWithIndependentRoundedCorners.cs
+++ UiRoundedCorners/ImageWithIndependentRoundedCorners.cs
@@ -1,38 +1,44 @@
 ﻿using Nobi.UiRoundedCorners;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Nobi.UiRoundedCorners {
+namespace Nobi.UiRoundedCorners
+{
     [ExecuteInEditMode]								//Required to do validation with OnEnable()
     [DisallowMultipleComponent]                     //You can only have one of these in every object
     [RequireComponent(typeof(RectTransform))]
-	public class ImageWithIndependentRoundedCorners : MonoBehaviour {
-		private static readonly int prop_halfSize = Shader.PropertyToID("_halfSize");
-		private static readonly int prop_radiuses = Shader.PropertyToID("_r");
-		private static readonly int prop_rect2props = Shader.PropertyToID("_rect2props");
+    public class ImageWithIndependentRoundedCorners : MonoBehaviour
+    {
+        private static readonly int prop_halfSize = Shader.PropertyToID("_halfSize");
+        private static readonly int prop_radiuses = Shader.PropertyToID("_r");
+        private static readonly int prop_rect2props = Shader.PropertyToID("_rect2props");
 
-		// Vector2.right rotated clockwise by 45 degrees
-		private static readonly Vector2 wNorm = new Vector2(.7071068f, -.7071068f);
-		// Vector2.right rotated counter-clockwise by 45 degrees
-		private static readonly Vector2 hNorm = new Vector2(.7071068f, .7071068f);
+        // Vector2.right rotated clockwise by 45 degrees
+        private static readonly Vector2 wNorm = new Vector2(.7071068f, -.7071068f);
+        // Vector2.right rotated counter-clockwise by 45 degrees
+        private static readonly Vector2 hNorm = new Vector2(.7071068f, .7071068f);
 
         public Vector4 r = new Vector4(40f, 40f, 40f, 40f);
         private Material material;
 
-		// xy - position,
-		// zw - halfSize
-		[HideInInspector, SerializeField] private Vector4 rect2props;
-		[HideInInspector, SerializeField] private MaskableGraphic image;
+        // xy - position,
+        // zw - halfSize
+        [HideInInspector, SerializeField] private Vector4 rect2props;
+        [HideInInspector, SerializeField] private MaskableGraphic image;
 
-		private void OnValidate() {
-			Validate();
-			Refresh();
-		}
+        private void OnValidate()
+        {
+            Validate();
+            Refresh();
+        }
 
-		private void OnEnable() {
+        private void OnEnable()
+        {
             //You can only add either ImageWithRoundedCorners or ImageWithIndependentRoundedCorners
-			//It will replace the other component when added into the object.
+            //It will replace the other component when added into the object.
             var other = GetComponent<ImageWithRoundedCorners>();
             if (other != null)
             {
@@ -41,79 +47,98 @@ namespace Nobi.UiRoundedCorners {
             }
 
             Validate();
-			Refresh();
-		}
+            Refresh();
+        }
 
-		private void OnRectTransformDimensionsChange() {
-			if (enabled && material != null) {
-				Refresh();
-			}
-		}
+        private void OnRectTransformDimensionsChange()
+        {
+            if (enabled && material != null)
+            {
+                Refresh();
+            }
+        }
 
-		private void OnDestroy() {
+        private void OnDestroy()
+        {
             image.material = null;      //This makes so that when the component is removed, the UI material returns to null
 
             DestroyHelper.Destroy(material);
-			image = null;
-			material = null;
-		}
+            image = null;
+            material = null;
+        }
 
-		public void Validate() {
-			if (material == null) {
-				material = new Material(Shader.Find("UI/RoundedCorners/IndependentRoundedCorners"));
-			}
+        public void Validate()
+        {
+            var isDirty = false;
+            if (material == null)
+            {
+                material = new Material(Shader.Find("UI/RoundedCorners/IndependentRoundedCorners"));
+                isDirty = true;
+            }
 
-			if (image == null) {
-				TryGetComponent(out image);
-			}
+            if (image == null)
+            {
+                TryGetComponent(out image);
+                isDirty = true;
+            }
 
-			if (image != null) {
-				image.material = material;
-			}
-		}
+            if (image != null)
+            {
+                image.material = material;
+                isDirty = true;
+            }
+#if UNITY_EDITOR
+            if (!isDirty)
+            {
+                EditorUtility.SetDirty(gameObject);
+            }
+#endif
+        }
 
-		public void Refresh() {
-			var rect = ((RectTransform)transform).rect;
-			RecalculateProps(rect.size);
-			material.SetVector(prop_rect2props, rect2props);
-			material.SetVector(prop_halfSize, rect.size * .5f);
-			material.SetVector(prop_radiuses, r);
-		}
+        public void Refresh()
+        {
+            var rect = ((RectTransform)transform).rect;
+            RecalculateProps(rect.size);
+            material.SetVector(prop_rect2props, rect2props);
+            material.SetVector(prop_halfSize, rect.size * .5f);
+            material.SetVector(prop_radiuses, r);
+        }
 
-		private void RecalculateProps(Vector2 size) {
-			// Vector that goes from left to right sides of rect2
-			var aVec = new Vector2(size.x, -size.y + r.x + r.z);
+        private void RecalculateProps(Vector2 size)
+        {
+            // Vector that goes from left to right sides of rect2
+            var aVec = new Vector2(size.x, -size.y + r.x + r.z);
 
-			// Project vector aVec to wNorm to get magnitude of rect2 width vector
-			var halfWidth = Vector2.Dot(aVec, wNorm) * .5f;
-			rect2props.z = halfWidth;
-
-
-			// Vector that goes from bottom to top sides of rect2
-			var bVec = new Vector2(size.x, size.y - r.w - r.y);
-
-			// Project vector bVec to hNorm to get magnitude of rect2 height vector
-			var halfHeight = Vector2.Dot(bVec, hNorm) * .5f;
-			rect2props.w = halfHeight;
+            // Project vector aVec to wNorm to get magnitude of rect2 width vector
+            var halfWidth = Vector2.Dot(aVec, wNorm) * .5f;
+            rect2props.z = halfWidth;
 
 
-			// Vector that goes from left to top sides of rect2
-			var efVec = new Vector2(size.x - r.x - r.y, 0);
+            // Vector that goes from bottom to top sides of rect2
+            var bVec = new Vector2(size.x, size.y - r.w - r.y);
 
-			// Vector that goes from point E to point G, which is top-left of rect2
-			var egVec = hNorm * Vector2.Dot(efVec, hNorm);
+            // Project vector bVec to hNorm to get magnitude of rect2 height vector
+            var halfHeight = Vector2.Dot(bVec, hNorm) * .5f;
+            rect2props.w = halfHeight;
 
-			// Position of point E relative to center of coord system
-			var ePoint = new Vector2(r.x - (size.x / 2), size.y / 2);
 
-			// Origin of rect2 relative to center of coord system
-			// ePoint + egVec == vector to top-left corner of rect2
-			// wNorm * halfWidth + hNorm * -halfHeight == vector from top-left corner to center
-			var origin = ePoint + egVec + wNorm * halfWidth + hNorm * -halfHeight;
-			rect2props.x = origin.x;
-			rect2props.y = origin.y;
-		}
-	}
+            // Vector that goes from left to top sides of rect2
+            var efVec = new Vector2(size.x - r.x - r.y, 0);
+
+            // Vector that goes from point E to point G, which is top-left of rect2
+            var egVec = hNorm * Vector2.Dot(efVec, hNorm);
+
+            // Position of point E relative to center of coord system
+            var ePoint = new Vector2(r.x - (size.x / 2), size.y / 2);
+
+            // Origin of rect2 relative to center of coord system
+            // ePoint + egVec == vector to top-left corner of rect2
+            // wNorm * halfWidth + hNorm * -halfHeight == vector from top-left corner to center
+            var origin = ePoint + egVec + wNorm * halfWidth + hNorm * -halfHeight;
+            rect2props.x = origin.x;
+            rect2props.y = origin.y;
+        }
+    }
 }
 
 /// <summary>
--- UiRoundedCorners/ImageWithRoundedCorners.cs
+++ UiRoundedCorners/ImageWithRoundedCorners.cs
@@ -1,32 +1,40 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
 using UnityEngine.UI;
 
-namespace Nobi.UiRoundedCorners {
+namespace Nobi.UiRoundedCorners
+{
     [ExecuteInEditMode]								//Required to check the OnEnable function
     [DisallowMultipleComponent]                     //You can only have one of these in every object.
     [RequireComponent(typeof(RectTransform))]
-	public class ImageWithRoundedCorners : MonoBehaviour {
-		private static readonly int Props = Shader.PropertyToID("_WidthHeightRadius");
+    public class ImageWithRoundedCorners : MonoBehaviour
+    {
+        private static readonly int Props = Shader.PropertyToID("_WidthHeightRadius");
 
-        public float radius = 40f;          
+        public float radius = 40f;
         private Material material;
 
-		[HideInInspector, SerializeField] private MaskableGraphic image;
+        [HideInInspector, SerializeField] private MaskableGraphic image;
 
-		private void OnValidate() {
-			Validate();
-			Refresh();
-		}
+        private void OnValidate()
+        {
+            Validate();
+            Refresh();
+        }
 
-		private void OnDestroy() {
+        private void OnDestroy()
+        {
             image.material = null;      //This makes so that when the component is removed, the UI material returns to null
 
             DestroyHelper.Destroy(material);
-			image = null;
-			material = null;
-		}
+            image = null;
+            material = null;
+        }
 
-		private void OnEnable() {
+        private void OnEnable()
+        {
             //You can only add either ImageWithRoundedCorners or ImageWithIndependentRoundedCorners
             //It will replace the other component when added into the object.
             var other = GetComponent<ImageWithIndependentRoundedCorners>();
@@ -37,35 +45,52 @@ namespace Nobi.UiRoundedCorners {
             }
 
             Validate();
-			Refresh();
-		}
+            Refresh();
+        }
 
-		private void OnRectTransformDimensionsChange() {
-			if (enabled && material != null) {
-				Refresh();
-			}
-		}
+        private void OnRectTransformDimensionsChange()
+        {
+            if (enabled && material != null)
+            {
+                Refresh();
+            }
+        }
 
-		public void Validate() {
-			if (material == null) {
-				material = new Material(Shader.Find("UI/RoundedCorners/RoundedCorners"));
-			}
+        public void Validate()
+        {
+            var isDirty = false;
+            if (material == null)
+            {
+                material = new Material(Shader.Find("UI/RoundedCorners/RoundedCorners"));
+                isDirty = true;
+            }
 
-			if (image == null) {
-				TryGetComponent(out image);
-			}
+            if (image == null)
+            {
+                TryGetComponent(out image);
+                isDirty = true;
+            }
 
-			if (image != null) {
-				image.material = material;
-			}
-		}
+            if (image != null)
+            {
+                image.material = material;
+                isDirty = true;
+            }
+#if UNITY_EDITOR
+            if (isDirty)
+            {
+                EditorUtility.SetDirty(gameObject);
+            }
+#endif
+        }
 
-		public void Refresh() {
-			var rect = ((RectTransform)transform).rect;
+        public void Refresh()
+        {
+            var rect = ((RectTransform)transform).rect;
 
             //Multiply radius value by 2 to make the radius value appear consistent with ImageWithIndependentRoundedCorners script.
             //Right now, the ImageWithIndependentRoundedCorners appears to have double the radius than this.
-            material.SetVector(Props, new Vector4(rect.width, rect.height, radius * 2, 0));   
+            material.SetVector(Props, new Vector4(rect.width, rect.height, radius * 2, 0));
         }
-	}
+    }
 }
